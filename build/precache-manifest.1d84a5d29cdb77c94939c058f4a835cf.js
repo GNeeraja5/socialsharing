db26ad3c84d118fb006a811b35c0a346ac8fd6d0
Neeraja Garigipati <v-negari@microsoft.com>
2019-02-13 12:58:30 -0800

    add static files

---
+++ build/precache-manifest.1d84a5d29cdb77c94939c058f4a835cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "688832ca2331a33b4dcd",
+    "url": "/static/js/main.688832ca.chunk.js"
+  },
+  {
+    "revision": "f3420ab362a760b48b09",
+    "url": "/static/js/1.f3420ab3.chunk.js"
+  },
+  {
+    "revision": "688832ca2331a33b4dcd",
+    "url": "/static/css/main.7369ba19.chunk.css"
+  },
+  {
+    "revision": "2959df7ea670e98e0c142985512c9cb5",
+    "url": "/index.html"
+  }
+];
